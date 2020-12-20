9b8fa0349957bec9f0230fe86238ba1e6b4106c4
Grit <perepadin00@gmail.com>
2020-12-20 17:10:27 +0300

    thirdCommit

--- scipt.js
+++ scipt.js
@@ -2,6 +2,58 @@
 
 // const result = confirm("Are you here?");
 // console.log(result);
- const user = "Ivan";
 
- alert(`Привет, ${user}`);
+console.log('arr' + " - object");
+
+if (4 == 5) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+
+(num === 50) ? console.log('Ok!') : console.log('Error');
+
+const num = 50;
+
+switch (num) {
+    case 49: 
+    console.log('Неверно');
+    break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('V tochku');
+        break;
+    default:
+        console.log('ne v etot raz');
+        break;
+}
+
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+let num = 50;
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+   if (i===6) {
+       continue;
+   }
+   console.log(i);
+}
+
+
+function showFirstMessage() {
+    console.log("Hello World!");
+}
+showFirstMessage();
